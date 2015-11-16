4d8fca43f247482fd56e56703ce6b66cdb9bd0ac
SudarsanGP <sudarsangp@gmail.com>
2015-11-16 19:42:09 +0800

    basic boiler plate angular wrapper

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name codeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the codeApp
+ */
+angular.module('codeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
