ba4aba4f83014658674a1cd04e45ec46a072ab8f
SudarsanGP <sudarsangp@gmail.com>
2015-11-29 22:26:20 +0800

    able to add and remove single habit in e2e tests

--- test/e2e/basic-worfkflows.js
+++ test/e2e/basic-worfkflows.js
@@ -53,8 +53,27 @@ describe('basic habit workflows', function() {
   //   });
   // });
 
-  // css class fa fa-times does not get added
-  // describe('adding and removing habits: ', function(){
+  describe('adding and removing habits: ', function(){
+    beforeEach(function() {
+      browser.get('index.html');
+    });
+
+    it('should be able to add a habit and remove a habit', function(){
+      var query = element(by.model('main.habitName'));
+      query.sendKeys('first habit');
+      element(by.buttonText('Add')).click();
+
+      var habitList = element.all(by.repeater('habit in main.habits'));
+      element.all(by.repeater('habit in main.habits')).then(function(habits){
+        habits[0].element(by.className('glyphicon-remove')).click();
+        browser.switchTo().alert().accept();
+        expect(habitList.count()).toBe(0);
+      });
+    });
+  });
+
+  // removing multiple habits
+  // describe('adding and removing multiple habits: ', function(){
   //   beforeEach(function() {
   //     browser.get('index.html');
   //   });
@@ -63,14 +82,20 @@ describe('basic habit workflows', function() {
   //     var query = element(by.model('main.habitName'));
   //     query.sendKeys('first habit');
   //     element(by.buttonText('Add')).click();
+  //     query.sendKeys('second habit');
+  //     element(by.buttonText('Add')).click();
+
   //     var habitList = element.all(by.repeater('habit in main.habits'));
-  //     // var addedHabit = element.all(by.css('fa fa-times')).first();
-  //     // addedHabit.click();
-  //     // expect(habitList.count()).toBe(0);
-  //     // expect(element(by.css('fa fa-times'))).toBe(true);
   //     element.all(by.repeater('habit in main.habits')).then(function(habits){
-  //       habits[0].element(by.className('fa-times')).click();
-  //       expect(habitList.count()).toBe(0);
+  //       // for(var i=0; i<habits.length; i++){
+  //         console.log(habits.length);
+  //         habits[0].element(by.className('glyphicon-remove')).click();
+  //         browser.switchTo().alert().accept();
+  //         expect(habitList.count()).toBe(1);
+  //       // }
+  //         habits[0].element(by.className('glyphicon-remove')).click();
+  //         browser.switchTo().alert().accept();
+  //         expect(habitList.count()).toBe(0);
   //     });
   //   });
   // });
