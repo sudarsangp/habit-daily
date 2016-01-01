dacf904e84b65b030844fb625a36604c7f737d02
SudarsanGP <sudarsangp@gmail.com>
2016-01-01 11:02:03 +0800

    habit end points work for get, post, put, delete

--- server/rest-api.py
+++ server/rest-api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, abort
 from flask.ext.restful import Api, Resource, reqparse, fields, marshal
 
 app = Flask(__name__)
@@ -10,28 +10,17 @@ status_fields = {
 }
 
 habit_fields = {
+	'id': fields.Integer,
 	'name': fields.String,
 	'streak': fields.Integer,
 	'created': fields.Integer,
 	'status': fields.List(fields.Nested(status_fields)),
 	'state': fields.List(fields.Integer),
-	'current': fields.List(fields.Integer)
+	'current': fields.List(fields.Integer),
+	'uri': fields.Url('habit')
 }
 
-test_habits = [
-	{
-		'id': 1,
-		'name': 'first',
-		'status': {
-			'started': 10001,
-			'finished': 10002
-		}
-	},
-	{
-		'id': 2,
-		'name': 'second'
-	}
-]
+test_habits = []
 
 class HabitListAPI(Resource):
 	def __init__(self):
@@ -49,8 +38,9 @@ class HabitListAPI(Resource):
 
 	def post(self):
 		args = self.reqparse.parse_args()
+		habit_id = test_habits[-1]['id'] + 1 if len(test_habits) > 0 else 0
 		habit = {
-			'id': test_habits[-1]['id'] + 1,
+			'id': habit_id,
 			'name': args['name'],
 			'streak': args['streak'],
 			'created': args['created'],
@@ -62,20 +52,50 @@ class HabitListAPI(Resource):
 		return {'task': marshal(habit, habit_fields)}, 201
 
 class HabitAPI(Resource):
+	def __init__(self):
+		self.reqparse = reqparse.RequestParser()
+		self.reqparse.add_argument('name', type = str,  location = 'json')
+		self.reqparse.add_argument('streak', type = int,  location = 'json')
+		self.reqparse.add_argument('created', type = int,  location = 'json')
+		self.reqparse.add_argument('status', type = list,  location = 'json')
+		self.reqparse.add_argument('state', type = list,  location = 'json')
+		self.reqparse.add_argument('current', type = list,  location = 'json')
+		super(HabitAPI, self).__init__()
+
 	def get(self, id):
-		pass
+		habit = [habit for habit in test_habits if habit['id'] == id]
+		if len(habit) == 0:
+			abort(404)
+		return {'habit': marshal(habit[0], habit_fields)}
 
 	def put(self, id):
-		pass
+		habit = [habit for habit in test_habits if habit['id'] == id]
+		if len(habit) == 0:
+			abort(404)
+		habit = habit[0]
+		args = self.reqparse.parse_args()
+		for k,v in args.items():
+			if v is not None:
+				habit[k] = v
+		return {'habit': marshal(habit, habit_fields)}
 
 	def delete(self, id):
-		pass
+		habit = [habit for habit in test_habits if habit['id'] == id]
+		if len(habit) == 0:
+			abort(404)
+		test_habits.remove(habit[0])
+		return {'result': True}
 
 api.add_resource(HabitListAPI, '/habitdaily/api/v1.0/habits', endpoint='habits')
 api.add_resource(HabitAPI, '/habitdaily/api/v1.0/habits/<int:id>', endpoint='habit')
 
-"""curl -i http://127.0.0.1:8000/habitdaily/api/v1.0/habits GET REQUEST
+"""HABITS END POINT
+	 curl -i http://127.0.0.1:8000/habitdaily/api/v1.0/habits GET REQUEST
 	 curl -i -H "Content-Type: application/json" -X POST -d @singlehabit http://127.0.0.1:8000/habitdaily/api/v1.0/habits
+	 HABIT END POINT
+	 curl -i -H "Content-Type: application/json" -X PUT -d '{"name": "habit changed from curl"}'  http://127.0.0.1:8000/habitdaily/api/v1.0/habits/0
+	 curl -i http://127.0.0.1:8000/habitdaily/api/v1.0/habits/0 GET REQUEST
+	 curl -i -X DELETE http://127.0.0.1:8000/habitdaily/api/v1.0/habits/0
 """
 
 app.run(host='127.0.0.1', port=8000, debug=True)
