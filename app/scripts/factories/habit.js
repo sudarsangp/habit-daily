9364920eb0c3426f69424f3984ea1424bc450579
SudarsanGP <sudarsangp@gmail.com>
2016-03-03 09:46:01 +0800

    fix: used a different logic to calculate date difference

--- app/scripts/factories/habit.js
+++ app/scripts/factories/habit.js
@@ -36,9 +36,9 @@ angular.module('codeApp')
 			habit.name = habitData.name;
       habit.streak = habitData.streak;
       habit.created = moment(habitData.created * 1000).toDate();
-
+      console.log(dateDiffInDays( new Date(habit.created), today));
       if(moment(habit.created).date() !== moment(today).date()){
-        if(numbers <= (moment(moment(today) - moment(habit.created)).date())) {
+        if(numbers <= (dateDiffInDays( new Date(habit.created), today))) {
         	if(typeof Token.getRefreshToken() === 'undefined'){
         		LocalStorageService.newDayModifyHabit(habit.id);
         	} else{
@@ -90,5 +90,14 @@ angular.module('codeApp')
 			return status;
 		}
 
+		var _MS_PER_DAY = 1000 * 60 * 60 * 24;
+		// a and b are javascript Date objects
+		function dateDiffInDays(a, b) {
+		  // Discard the time and time-zone information.
+		  var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+		  var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+
+		  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+		}
 		return Habit;
 	});
